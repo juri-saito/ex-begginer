7055092cc46066f25751cbb8d5a3eb4cda4cb9f1
jurisaito <juri.saito@rakus-partners.co.jp>
2019-08-01 16:36:28 +0900

    ymlファイルのコンテキストパスを修正。exam01.htmlを新規作成。

--- src/main/java/com/example/controller/Exam01Controller.java
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/Exam01")
+@RequestMapping("/ex01")
 public class Exam01Controller {
 
 	@RequestMapping("")
